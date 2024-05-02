67e0d127d96dc53f5516dae39426433f80d18cf9
Fabricio <fabridp77@gmail.com>
2024-05-02 11:20:50 -0300

    navbars-fixed

--- src/components/BranchNavbar.svelte
+++ src/components/BranchNavbar.svelte
@@ -1,16 +1,16 @@
 <script>
 	import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
-	import { CodePullRequestSolid } from 'flowbite-svelte-icons';
+	import { CloudArrowUpSolid, CodePullRequestSolid, GridSolid } from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
 
 	export let darkModed;
-    let darked = true;
+	let darked = true;
 	let beganBlack = true;
 
 	$: {
-		if(beganBlack){
+		if (beganBlack) {
 			darked = darkModed;
-		}else{
+		} else {
 			darked = !darkModed;
 		}
 	}
@@ -20,8 +20,8 @@
 	});
 </script>
 
-<Navbar class="bg-gray-0 dark:bg-gray-600 shadow-xl">
-	<NavBrand class="w-40 m-2" href="/">
+<Navbar class="bg-gray-0 shadow-xl dark:bg-gray-600 z-40">
+	<NavBrand class="m-2 w-40" href="/">
 		{#if darked}
 			<img alt="logo" src="/dark-logo.svg" id="navbar-logo" />
 		{/if}
@@ -29,11 +29,10 @@
 			<img alt="logo" src="/logo.svg" id="navbar-logo" />
 		{/if}
 	</NavBrand>
-	<NavHamburger />
 	<NavUl>
-		<NavLi href="/" class="flex gap-2">
-            Sinc
-            <CodePullRequestSolid />
-        </NavLi>
+		<NavLi href="/" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+			<CloudArrowUpSolid size={'lg'} class="text-gray-300" />
+			<span class="ms-3 flex-1 whitespace-nowrap">Sincronizar</span>
+		</NavLi>
 	</NavUl>
 </Navbar>
